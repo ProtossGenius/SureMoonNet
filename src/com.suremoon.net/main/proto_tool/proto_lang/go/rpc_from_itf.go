8339c5565d4c7cecd40deef830504c451edae9be
suremoon <protossgenius@gmail.com>
2019-08-13 23:19:35 +0800

    little code

--- src/com.suremoon.net/main/proto_tool/proto_lang/go/rpc_from_itf.go
+++ src/com.suremoon.net/main/proto_tool/proto_lang/go/rpc_from_itf.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"com.suremoon.net/basis/smn_pglang"
 	"com.suremoon.net/smn/analysis/smn_rpc_itf"
-	"flag"
+	"com.suremoon.net/smn/code_file_build"
 )
 
 func check(err error) {
@@ -12,16 +16,42 @@ func check(err error) {
 	}
 }
 
-func writeRpcFile(path string, list []*smn_pglang.ItfDef) {
+/** file as:
+package xxxx
+import(...)
+
+*/
+func writeSvrRpcFile(path string, list []*smn_pglang.ItfDef) {
+	code_file_build.NewGoFile("", nil)
+	for _, itf := range list {
+		fmt.Println(itf.Package, "============")
+	}
+}
+
+func writeClientRpcFile(path string, list []*smn_pglang.ItfDef) {
+
 }
 
 func main() {
 	i := flag.String("i", "./src/rpc_itf/", "rpc interface dir.")
 	o := flag.String("o", "./src/rpc_nitf/", "rpc insterface;'s net accepter, from proto.Message call interface.")
+	s := flag.Bool("s", false, "is product server code")
+	c := flag.Bool("c", false, "is product client code")
 	flag.Parse()
 	itfs, err := smn_rpc_itf.GetItfListFromDir(*i)
 	check(err)
-	for pkg, list := range itfs {
-		writeRpcFile(*o+"/rpc_"+pkg+".go", list)
+	for _, list := range itfs {
+		if *s {
+			op := *o + "/svrrpc/"
+			err := os.MkdirAll(op, os.ModePerm)
+			check(err)
+			writeSvrRpcFile(op, list)
+		}
+		if *c {
+			op := *o + "/clientrpc/"
+			err := os.MkdirAll(op, os.ModePerm)
+			check(err)
+			writeClientRpcFile(op, list)
+		}
 	}
 }
