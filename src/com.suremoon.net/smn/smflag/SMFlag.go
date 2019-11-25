109c64a090d461bee9c0e187347f1bf09be07aba
suremoon <protossgenius@gmail.com>
2019-11-25 16:54:02 +0800

    about flag but not succsess

--- src/com.suremoon.net/smn/smflag/SMFlag.go
+++ src/com.suremoon.net/smn/smflag/SMFlag.go
@@ -1,1 +1,22 @@
 package smflag
+
+import (
+	"flag"
+	"reflect"
+)
+
+type SMFlagValueItf interface {
+	Parse()
+}
+
+type SMFlag struct {
+	Value   SMFlagValueItf
+	CmdList []string
+}
+
+func Parse(value SMFlagValueIt) *SMFlag {
+	res := &SMFlag{Value: value}
+	tv := reflect.TypeOf(value)
+	flag.Parse()
+	return res
+}
--- src/com.suremoon.net/test/smflag/testflag.go
+++ src/com.suremoon.net/test/smflag/testflag.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"strings"
+	"reflect"
 )
 
+type StructTest struct {
+	Hello string `fd:"about desc"`
+}
+
 func main() {
-	flag.String("n", "name", "no use")
-	flag.Parse()
-	fmt.Println(flag.Args())
-	fmt.Println("vim-go")
-	idx := strings.Index("hello=\"world\"", "=")
-	fmt.Println(idx)
+	t := &StructTest{Hello: "hello world"}
+	tt := reflect.TypeOf(*t)
+	vt := reflect.ValueOf(t).Elem()
+	//	tvt := vt.Type()
+	for i := 0; i < tt.NumField(); i++ {
+		ft := tt.Field(i)
+		fmt.Printf("name %v tag %v type %v\n", ft.Name, ft.Tag, ft.Type)
+		v := vt.Field(i).Interface()
+		fmt.Printf("~~~~~~~~~~~~~ %v", v)
+		v = "ccccccccccccc"
+		fmt.Printf(t.Hello)
+	}
 }
