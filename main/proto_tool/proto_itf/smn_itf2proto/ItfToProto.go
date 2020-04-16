d80aae6662e21acacd2947a0b90b6500c70219c6
protossgenius <guyvejianglou@outlook.com>
2020-04-16 22:18:56 +0800

    ""add Itf2Proto funcs to smn/proto_tool/itf2proto"

--- main/proto_tool/proto_itf/smn_itf2proto/ItfToProto.go
+++ main/proto_tool/proto_itf/smn_itf2proto/ItfToProto.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
 	"os"
-	"strings"
 
-	"github.com/ProtossGenius/SureMoonNet/basis/smn_file"
-	"github.com/ProtossGenius/SureMoonNet/basis/smn_muti_write_cache"
-	"github.com/ProtossGenius/SureMoonNet/basis/smn_pglang"
-	"github.com/ProtossGenius/SureMoonNet/basis/smn_str"
 	"github.com/ProtossGenius/SureMoonNet/smn/analysis/smn_rpc_itf"
+	"github.com/ProtossGenius/SureMoonNet/smn/proto_tool/itf2proto"
 )
 
 func checkerr(err error) {
@@ -20,77 +14,15 @@ func checkerr(err error) {
 	}
 }
 
-func writeProto(oPath string, list []*smn_pglang.ItfDef) {
-	pkg := list[0].Package
-	file, err := smn_file.CreateNewFile(oPath)
-	checkerr(err)
-	w := smn_muti_write_cache.NewFileMutiWriteCache()
-	writeLine := func(f string, a ...interface{}) {
-		w.WriteTail(fmt.Sprintf(f, a...) + "\n")
-	}
-	w.Append(smn_muti_write_cache.NewStrCache(fmt.Sprintf("syntax = \"proto3\";\noption java_package = \"pb\";\noption java_outer_classname=\"%s\";\npackage rip_%s;\n", pkg, pkg)))
-	impts := smn_muti_write_cache.NewStrCache()
-	impMap := make(map[string]bool)
-	checkImport := func(typ string) {
-		if typ == "net.Conn" {
-			return
-		}
-		nameList := strings.Split(typ, ".")
-		if len(nameList) == 1 {
-			return
-		}
-		if !impMap[nameList[0]] {
-			impts.WriteTail(fmt.Sprintf("import \"%s.proto\";", nameList[0]) + "\n")
-			impMap[nameList[0]] = true
-		}
-	}
-	w.Append(impts)
-	for _, itf := range list {
-		for _, mtd := range itf.Functions {
-			if mtd.Name[0] < 'A' || mtd.Name[0] > 'Z' {
-				log.Printf("warning! mtd.Name %s's first letter not upper\n", mtd.Name)
-				continue
-			}
-			writeLine("message %s_%s_Prm {", itf.Name, mtd.Name)
-			for i, prm := range mtd.Params {
-				isArray, typ := smn_str.ProtoUseDeal(prm.Type)
-				if typ == "net.Conn" {
-					continue
-				}
-				checkImport(typ)
-				rpt := ""
-				if isArray {
-					rpt = "repeated "
-				}
-				writeLine("\t%s%s %s = %d;", rpt, typ, prm.Var, i+1)
-			}
-			writeLine("}")
-			writeLine("message %s_%s_Ret {", itf.Name, mtd.Name)
-			for i, res := range mtd.Returns {
-				isArray, typ := smn_str.ProtoUseDeal(res.Type)
-				checkImport(typ)
-				rpt := ""
-				if isArray {
-					rpt = "repeated "
-				}
-				writeLine("\t%s%s %s = %d;", rpt, typ, res.Var, i+1)
-			}
-			writeLine("}")
-		}
-	}
-	w.Output(file)
-}
-
 func main() {
 	i := flag.String("i", "./rpc_itf/", "rpc interface dir.")
-	o := flag.String("o", "./datas/proto/", "rpc needs proto output.")
+	o := flag.String("o", "./datas/proto/", "proto output dir.")
 	flag.Parse()
 	err := os.MkdirAll(*o, os.ModePerm)
 	checkerr(err)
 	itfs, err := smn_rpc_itf.GetItfListFromDir(*i)
 	checkerr(err)
 	for _, list := range itfs {
-		pkg := list[0].Package
-		writeProto(*o+"/rip_"+pkg+".proto", list)
+		itf2proto.WriteProto(*o, list)
 	}
 }
