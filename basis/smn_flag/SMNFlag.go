0e381673ea1c2b73b7b7d18561f72dca3c31ac97
s.m <guyejianglou@outlook.com>
2020-05-29 19:08:03 +0800

    "some change about smn_flag, let it near right."

--- basis/smn_flag/SMNFlag.go
+++ basis/smn_flag/SMNFlag.go
@@ -3,6 +3,7 @@ package smn_flag
 import (
 	"flag"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/ProtossGenius/SureMoonNet/basis/smn_err"
@@ -10,6 +11,7 @@ import (
 
 type smnFlag struct {
 	SFValRegMap map[string]*sFvalReg
+	Args        []string
 }
 
 type sFvalReg struct {
@@ -18,16 +20,38 @@ type sFvalReg struct {
 	Func    ActionDo
 }
 
-func (this *sFvalReg) GetValue(str string) {
+func (this *sFvalReg) SetValue(str string) {
 	if this.StrPtr != nil {
 		*this.StrPtr = str
 	}
+
 	if this.BoolPtr != nil {
-		*this.BoolPtr = (str == "true" || str == "t" || str == "")
+		*this.BoolPtr = (str == "true" || str == "t" || str == "" || str == "1")
 	}
 }
 
-type ActionDo func(args []string) error
+func (this *sFvalReg) GetValue() string {
+	if this.StrPtr != nil {
+		return *this.StrPtr
+	}
+
+	if this.BoolPtr != nil {
+		return strconv.FormatBool(*this.BoolPtr)
+	}
+
+	return ""
+}
+
+//GetRegValue get registered value.
+func GetRegValue(key string) *sFvalReg {
+	return _smnFlag.SFValRegMap[key]
+}
+
+func FlagArgs() []string {
+	return _smnFlag.Args
+}
+
+type ActionDo func(val string) error
 
 func newsmnFlag() *smnFlag {
 	res := &smnFlag{SFValRegMap: map[string]*sFvalReg{}}
@@ -36,22 +60,28 @@ func newsmnFlag() *smnFlag {
 
 var _smnFlag = newsmnFlag()
 
-func (this *smnFlag) RegisterString(name string, val *string, useage string, ad ActionDo) {
-	flag.StringVar(val, name, *val, useage)
+func (this *smnFlag) RegisterString(name string, val *string, usage string, ad ActionDo) {
+	flag.StringVar(val, name, *val, usage)
+
 	this.SFValRegMap[name] = &sFvalReg{StrPtr: val, Func: ad}
 }
 
-func RegisterString(name string, val *string, useage string, ad ActionDo) {
-	_smnFlag.RegisterString(name, val, useage, ad)
+func RegisterString(name string, usage string, ad ActionDo) {
+	var val string
+
+	_smnFlag.RegisterString(name, &val, usage, ad)
 }
 
-func (this *smnFlag) RegisterBool(name string, val *bool, useage string, ad ActionDo) {
-	flag.BoolVar(val, name, *val, useage)
+func (this *smnFlag) RegisterBool(name string, val *bool, usage string, ad ActionDo) {
+	flag.BoolVar(val, name, *val, usage)
+
 	this.SFValRegMap[name] = &sFvalReg{BoolPtr: val, Func: ad}
 }
 
-func RegisterBool(name string, val *bool, useage string, ad ActionDo) {
-	_smnFlag.RegisterBool(name, val, useage, ad)
+func RegisterBool(name string, usage string, ad ActionDo) {
+	var val bool
+
+	_smnFlag.RegisterBool(name, &val, usage, ad)
 }
 
 func Parse(args []string, ed *smn_err.ErrDeal) {
@@ -61,24 +91,28 @@ func Parse(args []string, ed *smn_err.ErrDeal) {
 func (this *smnFlag) Parse(args []string, ed *smn_err.ErrDeal) {
 	last := ""
 	newArgs := make([]string, 0, len(args))
+
 	for _, arg := range args {
 		flag := (arg != "-" && arg[0] == '-')
+
 		var val *sFvalReg
+
 		if last != "" {
 			val = this.SFValRegMap[last]
 			if val != nil {
 				if flag {
-					val.GetValue("")
+					val.SetValue("")
 				} else {
-					val.GetValue(arg)
+					val.SetValue(arg)
 				}
 			}
 		}
+
 		if flag {
 			if idx := strings.Index(arg, "="); idx != -1 {
 				val = this.SFValRegMap[arg[1:idx]]
 				if val != nil {
-					val.GetValue(arg[idx+1:])
+					val.SetValue(arg[idx+1:])
 				}
 			} else {
 				last = arg[1:]
@@ -86,23 +120,18 @@ func (this *smnFlag) Parse(args []string, ed *smn_err.ErrDeal) {
 		} else if last != "" {
 			newArgs = append(newArgs, arg)
 		}
-
 	}
+
+	this.Args = newArgs
+
 	for name, valReg := range this.SFValRegMap {
-		if valReg.Func == nil {
+		if valReg.Func == nil || valReg.GetValue() == "" {
 			continue
 		}
-		if valReg.StrPtr != nil {
-			if *(valReg.StrPtr) == "" {
-				continue
-			}
-		} else if valReg.BoolPtr != nil {
-			if !*(valReg.BoolPtr) {
-				continue
-			}
-		}
+
 		fmt.Println("dealing funcs .... ", name)
-		err := valReg.Func(newArgs)
+
+		err := valReg.Func(valReg.GetValue())
 		ed.OnErr(err)
 		fmt.Println("deal func ", name, " finish.")
 	}
