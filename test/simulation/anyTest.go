414975fc40d1ad7c365beef31aabe9ae08e4d201
s.m <guyejianglou@outlook.com>
2020-06-18 15:57:38 +0800

    "code for SMNRPC/C++ 's Client code product."

--- test/simulation/anyTest.go
+++ test/simulation/anyTest.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/ProtossGenius/SureMoonNet/basis/smn_file"
+	"github.com/ProtossGenius/SureMoonNet/basis/smn_pglang"
+	"github.com/ProtossGenius/SureMoonNet/smn/proto_tool/itf2rpc"
 )
 
 func check(err error) {
@@ -15,22 +14,8 @@ func check(err error) {
 }
 
 func main() {
-	str := "github.com/ProtossGenius/SureMoonNet/hellowrold"
-	str = strings.Replace(str, "github.com/ProtossGenius/SureMoonNet", "github.com/ProtossGenius/SureMoonNet", -1)
-	fmt.Println(str)
-	smn_file.DeepTraversalDir("./", func(path string, info os.FileInfo) smn_file.FileDoFuncResult {
-		if info.IsDir() || !strings.HasSuffix(info.Name(), ".go") {
-			return smn_file.FILE_DO_FUNC_RESULT_DEFAULT
-		}
-		fData, err := smn_file.FileReadAll(path)
-		check(err)
-		fStr := string(fData)
-		fStr = strings.Replace(fStr, "github.com/ProtossGenius/SureMoonNet", "github.com/ProtossGenius/SureMoonNet", -1)
-		f, err := smn_file.CreateNewFile(path)
-		check(err)
-		f.WriteString(fStr)
-		f.Close()
-		return smn_file.FILE_DO_FUNC_RESULT_DEFAULT
-	})
+	fmt.Println("start .. ")
 
+	prm := itf2rpc.ToCppParam([]*smn_pglang.VarDef{{Type: "[]int64", Var: "p0", ArrSize: -1}})
+	fmt.Println(prm)
 }
